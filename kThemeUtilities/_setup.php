04ffbc23e7260bfc962ea92a337d631842a5a225
= <=>
2021-04-02 04:58:39 -0400

    made sure that the theme settings uses the new template maker

--- kThemeUtilities/_setup.php
+++ kThemeUtilities/_setup.php
@@ -19,9 +19,10 @@ function getThemeSettings()
     global $configsArr;
     
     $configs = KConfigSet::createNewConfigs($configsArr);
+    $kTemplateMaker = new KTemplateMaker($configs->getConfig("templatesDirectory"));
     
     $binfo = new BrowserInfo();
-    $settings = new DBlackBoySettings($configs,$binfo );
+    $settings = new DBlackBoySettings($configs,$binfo ,$kTemplateMaker);
     return $settings;
 }
 
