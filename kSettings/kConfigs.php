5a41d48e60329cd0db805740c763056eb4759cae
= <=>
2021-04-05 22:11:19 -0400

    updated kconfigs to reflect new forms

--- kSettings/kConfigs.php
+++ kSettings/kConfigs.php
@@ -18,9 +18,8 @@ $configsArr = [
     ,"currentPageUrl"=>  getCurrentPageUrl() 
     ,"themeAdminPageTitle"=>"Kuaminika Simple Theme"
     ,"themeAdminPageSlug"=>"kuaminikaSimpleTheme_settings"
-    
     ,"mediaUrl_wp"=>get_template_directory_uri()
-    ,"siteTitle_default" => [
+    ,"siteTitle_default" => [//TODO: make this point to getbloginfo or somehting
         "en"=>"First and Finest Consulting Company in Business Industrial Optimisation",
         "fr"=>"La premi&egrave;re et meilleure soci&eacute;t&eacute; de conseil et d'optimisation industrielle en Afrique"        
     ]
@@ -28,32 +27,20 @@ $configsArr = [
     ,"adminAPINamespace"=>"w1/v1"
     ,"controllerPagePath"=>"controllerScripts"
     ,"adminFormFields"=>[
-        ["fieldName" =>"contactFormTitle" ,"fieldLabel"=>"Contact form title"],
-        ["fieldName" =>"slogan" ,"fieldLabel"=>"Slogan"],
-
-        ["fieldName" =>"firstNameField_label" ,"fieldLabel"=>"First name label msg"],
-        ["fieldName" =>"firstNameField_error" ,"fieldLabel"=>"First name error msg"],
-
-        ["fieldName" =>"lastNameField_label" ,"fieldLabel"=>"Last name label msg"],
-        ["fieldName" =>"lastNameField_error" ,"fieldLabel"=>"Last name error msg"],
-
-        ["fieldName" =>"companyField_label" ,"fieldLabel"=>"Company label msg"],
-      
-        ["fieldName" =>"jobTitleField_label" ,"fieldLabel"=>"Job title label msg"],
-        ["fieldName" =>"jobTitleField_error" ,"fieldLabel"=>"Job title error msg"],
-
-        ["fieldName" =>"languageField_label" ,"fieldLabel"=>"Language label msg"],
-
-        ["fieldName" =>"phone_label" ,"fieldLabel"=>"Phone label msg"],
-
-        ["fieldName" =>"emailField_label" ,"fieldLabel"=>"Email label msg"],
-        ["fieldName" =>"emailField_error" ,"fieldLabel"=>"Email error msg"],
-
-        ["fieldName" =>"subjectField_label" ,"fieldLabel"=>"Subject label msg"],
-
-        ["fieldName" =>"message_label" ,"fieldLabel"=>"Message label msg"],
-
-        ["fieldName" =>"submitBtn_label" ,"fieldLabel"=>"Send btn label msg"]
-
+        "socialMedia"=>[
+                            "name"=>"socialMedia",
+                            "fields"=>[
+                              ["fieldName" =>"instagram" ,"fieldLabel"=>"instagramUrl"]
+                            , ["fieldName" =>"twitter" ,"fieldLabel"=>"twitterUrl"]  
+                            ]   
+                        ],
+        "colorChoosers"=>[
+                            "name"=>"colorChoosers",
+                            "fields"=>[
+                              ["fieldName" =>"bgBarColorCode" ,"fieldLabel"=>"Background bars color code"]
+                            , ["fieldName" =>"headerbarFontColorCode" ,"fieldLabel"=>"Header bar color code"]  
+                            , ["fieldName" =>"BrandFontColorCode" ,"fieldLabel"=>"Brand color code"] 
+                            ]   
+                        ]
     ]
 ];
